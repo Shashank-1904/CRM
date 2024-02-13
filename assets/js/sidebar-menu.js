031f974844a41a9a711826366934fa56abcd678e
Shashank <shashankumarekar@gmail.com>
2024-02-13 21:27:42 +0530

    sidebar

--- assets/js/sidebar-menu.js
+++ assets/js/sidebar-menu.js
@@ -14,65 +14,7 @@
   }
 
   // left sidebar and vertical menu
-  if ($("#pageWrapper").hasClass("compact-wrapper")) {
-    jQuery(".sidebar-title").append(
-      '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-    );
-    jQuery(".sidebar-title").click(function () {
-      jQuery(".sidebar-title")
-        .removeClass("active")
-        .find("div")
-        .replaceWith(
-          '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-        );
-      jQuery(".sidebar-submenu, .menu-content").slideUp("normal");
-      jQuery(".menu-content").slideUp("normal");
-      if (jQuery(this).next().is(":hidden") == true) {
-        jQuery(this).addClass("active");
-        jQuery(this)
-          .find("div")
-          .replaceWith(
-            '<div class="according-menu"><i class="fa fa-angle-down"></i></div>'
-          );
-        jQuery(this).next().slideDown("normal");
-      } else {
-        jQuery(this)
-          .find("div")
-          .replaceWith(
-            '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-          );
-      }
-    });
-    jQuery(".sidebar-submenu, .menu-content").hide();
-    jQuery(".submenu-title").append(
-      '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-    );
-    jQuery(".submenu-title").click(function () {
-      jQuery(".submenu-title")
-        .removeClass("active")
-        .find("div")
-        .replaceWith(
-          '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-        );
-      jQuery(".submenu-content").slideUp("normal");
-      if (jQuery(this).next().is(":hidden") == true) {
-        jQuery(this).addClass("active");
-        jQuery(this)
-          .find("div")
-          .replaceWith(
-            '<div class="according-menu"><i class="fa fa-angle-down"></i></div>'
-          );
-        jQuery(this).next().slideDown("normal");
-      } else {
-        jQuery(this)
-          .find("div")
-          .replaceWith(
-            '<div class="according-menu"><i class="fa fa-angle-right"></i></div>'
-          );
-      }
-    });
-    jQuery(".submenu-content").hide();
-  } else if ($("#pageWrapper").hasClass("horizontal-wrapper")) {
+if ($("#pageWrapper").hasClass("horizontal-wrapper")) {
     var smallSize = false,
       bigSize = false;
     const horizontalMenu = () => {
