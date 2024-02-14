f79baa9bdcf6d850bfadfce0dabd5dcf9a339d74
Shashank <shashankumarekar@gmail.com>
2024-02-14 22:11:47 +0530

    header changed

--- Director/includes/header.php
+++ Director/includes/header.php
@@ -10,43 +10,43 @@
   <meta name="keywords"
     content="admin template, Mofi admin template, dashboard template, flat admin template, responsive admin template, web app">
   <meta name="author" content="pixelstrap">
-  <link rel="icon" href="../assets/images/logo/logoicon.png" type="image/x-icon">
-  <link rel="shortcut icon" href="../assets/images/logo/logoicon.png" type="image/x-icon">
+  <link rel="icon" href="../../assets/images/logo/logoicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="../../assets/images/logo/logoicon.png" type="image/x-icon">
   <title>CES CRM</title>
   <!-- Google font-->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
   <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
     rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/font-awesome.css">
   <!-- ico-font-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/icofont.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/icofont.css">
   <!-- Themify icon-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/themify.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/themify.css">
   <!-- Flag icon-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/flag-icon.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/flag-icon.css">
   <!-- Feather icon-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/feather-icon.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/feather-icon.css">
   <!-- Plugins css start-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/slick.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/slick-theme.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/scrollbar.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/animate.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/datatables.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/date-picker.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/owlcarousel.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/rating.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/vector-map.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/slick.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/scrollbar.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/animate.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/datatables.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/date-picker.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/owlcarousel.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/rating.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/vector-map.css">
   <!-- Plugins css Ends-->
   <!-- Bootstrap css-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/vendors/bootstrap.css">
   <!-- App css-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-  <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+  <link id="color" rel="stylesheet" href="../../assets/css/color-1.css" media="screen">
   <!-- Responsive css-->
-  <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
-  <link rel="stylesheet" href="../assets/css/topbar.css">
-  <link rel="stylesheet" href="../assets/css/dialpad.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/responsive.css">
+  <link rel="stylesheet" href="../../assets/css/topbar.css">
+  <link rel="stylesheet" href="../../assets/css/dialpad.css">
 </head>
 
 <body>
@@ -63,14 +63,14 @@
             <ul class="nav-menus">
               <li><span class="header-search">
                   <svg>
-                    <use href="../assets/svg/icon-sprite.svg#search"></use>
+                    <use href="../../assets/svg/icon-sprite.svg#search"></use>
                   </svg></span></li>
               <li>
                 <div class="form-group w-100">
                   <div class="Typeahead Typeahead--twitterUsers">
                     <div class="u-posRelative d-flex align-items-center">
                       <svg class="search-bg svg-color">
-                        <use href="../assets/svg/icon-sprite.svg#search"></use>
+                        <use href="../../assets/svg/icon-sprite.svg#search"></use>
                       </svg>
                       <input class="demo-input py-0 Typeahead-input form-control-plaintext w-100" type="text"
                         placeholder="Search Mofi .." name="q" title="">
@@ -81,13 +81,13 @@
               <li>
                 <div class="mode">
                   <svg>
-                    <use href="../assets/svg/icon-sprite.svg#moon"></use>
+                    <use href="../../assets/svg/icon-sprite.svg#moon"></use>
                   </svg>
                 </div>
               </li>
               <li class="profile-nav onhover-dropdown px-0 py-0">
                 <div class="d-flex profile-media align-items-center"><img class="img-30"
-                    src="../assets/images/dashboard/profile.png" alt="">
+                    src="../../assets/images/dashboard/profile.png" alt="">
                   <div class="flex-grow-1"><span>Alen Miller</span>
                     <p class="mb-0 font-outfit">UI Designer<i class="fa fa-angle-down"></i></p>
                   </div>
