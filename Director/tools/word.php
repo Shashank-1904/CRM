9669c211c4a25159c49a6f3943473f0571376772
Shashank <shashankumarekar@gmail.com>
2024-02-14 22:32:11 +0530

    director

--- Director/tools/word.php
+++ Director/tools/word.php
@@ -7,6 +7,7 @@ include('includes/toolbar.php');
 <div class="row">
     
           <div class="col-xl-9">
+            <!-- Word Actual Content -->
           </div>
           <div class="col-xl-3">
             <div class="container-dialpad">
