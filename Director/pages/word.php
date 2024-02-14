1c0ef01d653056421ba6c213ba2e0afd75611d77
Shashank <shashankumarekar@gmail.com>
2024-02-14 20:47:18 +0530

    word link

---
+++ Director/pages/word.php
@@ -0,0 +1,12 @@
+<?php
+include('includes/header.php');
+include('includes/sidebar.php');
+include('includes/toolbar.php');
+?>
+
+
+<?php
+include('includes/footer.php');
+include('includes/script.php');
+
+?>
