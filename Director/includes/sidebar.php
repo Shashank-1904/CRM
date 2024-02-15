b29177b6ed72200e4d58999bd5ccab32a6f675e0
Parth <bhawarparth@gmail.com>
2024-02-15 22:27:56 +0530

    .

--- Director/includes/sidebar.php
+++ Director/includes/sidebar.php
@@ -47,10 +47,10 @@
               <li class="sidebar-list"><i class="fa fa-thumb-tack"></i><a class="sidebar-link sidebar-title"
                   href="javascript:void(0)">
                   <svg class="stroke-icon">
-                    <use href="../assets/svg/solid/user.svg"></use>
+                    <use href="../assets/svg/icon-sprite.svg#stroke-user"></use>
                   </svg>
                   <svg class="fill-icon">
-                    <use href="../assets/svg/solid/user.svg"></use>
+                    <use href="../assets/svg/user.svg"></use>
                   </svg><span class="lan-30">Employee</span></a>
                 <ul class="sidebar-submenu">
                   <li><a href="employee_profile.php">Profile </a></li>
