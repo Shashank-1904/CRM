cbabd8a539ebdf37bf8c425f759345bb3f03bdca
Shashank <shashankumarekar@gmail.com>
2024-02-15 20:07:44 +0530

    s

--- Director/includes/header.php
+++ Director/includes/header.php
@@ -35,11 +35,11 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/vendors/date-picker.css">
   <link rel="stylesheet" type="text/css" href="../assets/css/vendors/owlcarousel.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/rating.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/vector-map.css">
+  <!-- <link rel="stylesheet" type="text/css" href="../assets/css/vendors/rating.css"> -->
+  <!-- <link rel="stylesheet" type="text/css" href="../assets/css/vendors/vector-map.css"> -->
   <link href="/styles.css" rel="stylesheet" />
-<link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.2/dist/quill.snow.css" rel="stylesheet" />
-<link   rel="stylesheet"   href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css"/>
+  <link href="https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.2/dist/quill.snow.css" rel="stylesheet" />
+  <link   rel="stylesheet"   href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css"/>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" />
   <!-- Plugins css Ends-->
   <!-- Bootstrap css-->
--- Director/includes/script.php
+++ Director/includes/script.php
@@ -18,26 +18,26 @@
   <script src="../assets/js/slick/slick.js"></script>
   <script src="../assets/js/header-slick.js"></script>
 
-  <script src="../assets/js/chart/morris-chart/raphael.js"></script>
-  <script src="../assets/js/chart/morris-chart/morris.js"> </script>
-  <script src="../assets/js/chart/morris-chart/prettify.min.js"></script>
-  <script src="../assets/js/chart/apex-chart/apex-chart.js"></script>
-  <script src="../assets/js/chart/apex-chart/stock-prices.js"></script>
-  <script src="../assets/js/chart/apex-chart/moment.min.js"></script>
-  <script src="../assets/js/chart/echart/pie-chart/facePrint.js"></script>
-  <script src="../assets/js/chart/echart/pie-chart/testHelper.js"></script>
-  <script src="../assets/js/chart/echart/pie-chart/custom-transition-texture.js"></script>
-  <script src="../assets/js/chart/echart/data/symbols.js"></script>
+  <!-- <script src="../assets/js/chart/morris-chart/raphael.js"></script> -->
+  <!-- <script src="../assets/js/chart/morris-chart/morris.js"> </script> -->
+  <!-- <script src="../assets/js/chart/morris-chart/prettify.min.js"></script> -->
+  <!-- <script src="../assets/js/chart/apex-chart/apex-chart.js"></script> -->
+  <!-- <script src="../assets/js/chart/apex-chart/stock-prices.js"></script> -->
+  <!-- <script src="../assets/js/chart/apex-chart/moment.min.js"></script> -->
+  <!-- <script src="../assets/js/chart/echart/pie-chart/facePrint.js"></script> -->
+  <!-- <script src="../assets/js/chart/echart/pie-chart/testHelper.js"></script> -->
+  <!-- <script src="../assets/js/chart/echart/pie-chart/custom-transition-texture.js"></script> -->
+  <!-- <script src="../assets/js/chart/echart/data/symbols.js"></script> -->
   <script src="../assets/js/slick/slick.min.js"></script>
   <script src="../assets/js/slick/slick-theme.js"></script>
-  <script src="../assets/js/vector-map/jquery-jvectormap-2.0.2.min.js"></script>
+  <!-- <script src="../assets/js/vector-map/jquery-jvectormap-2.0.2.min.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-world-mill-en.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-us-aea-en.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-uk-mill-en.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-au-mill.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-chicago-mill-en.js"></script>
   <script src="../assets/js/vector-map/map/jquery-jvectormap-in-mill.js"></script>
-  <script src="../assets/js/vector-map/map/jquery-jvectormap-asia-mill.js"></script>
+  <script src="../assets/js/vector-map/map/jquery-jvectormap-asia-mill.js"></script> -->
   
   <!-- calendar js-->
   <!-- <script src="../assets/js/datatable/datatables/jquery.dataTables.min.js"></script>
@@ -46,10 +46,10 @@
   <script src="../assets/js/datepicker/date-picker/datepicker.js"></script>
   <script src="../assets/js/datepicker/date-picker/datepicker.en.js"></script>
   <script src="../assets/js/datepicker/date-picker/datepicker.custom.js"></script>
-  <script src="../assets/js/rating/jquery.barrating.js"></script>
-  <script src="../assets/js/rating/rating-script.js"></script>
+  <!-- <script src="../assets/js/rating/jquery.barrating.js"></script>
+  <script src="../assets/js/rating/rating-script.js"></script> -->
   <script src="../assets/js/owlcarousel/owl.carousel.js"></script>
-  <script src="../assets/js/vector-map/map-vector.js"></script>
+  <!-- <script src="../assets/js/vector-map/map-vector.js"></script> -->
   <script src="../assets/js/countdown.js"></script>
   <script src="../assets/js/dashboard/dashboard_3.js"></script>
   <script src="../assets/js/ecommerce.js"></script>
