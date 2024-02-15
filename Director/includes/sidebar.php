a6886e521e53e698892dcda5f78a4656f157ae51
Parth <bhawarparth@gmail.com>
2024-02-15 22:22:47 +0530

    .

--- Director/includes/sidebar.php
+++ Director/includes/sidebar.php
@@ -47,7 +47,7 @@
               <li class="sidebar-list"><i class="fa fa-thumb-tack"></i><a class="sidebar-link sidebar-title"
                   href="javascript:void(0)">
                   <svg class="stroke-icon">
-                    <use href="../assets/svg/icon-sprite.svg#stroke-user"></use>
+                    <use href="../assets/svg/user.svg"></use>
                   </svg>
                   <svg class="fill-icon">
                     <use href="../assets/svg/user.svg"></use>
