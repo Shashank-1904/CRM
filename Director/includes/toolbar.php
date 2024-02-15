f5d65e47a60666f0ee461baadd871d4104568442
Aniket <aniketaher2002@gmail.com>
2024-02-15 20:45:43 +0530

    as

--- Director/includes/toolbar.php
+++ Director/includes/toolbar.php
@@ -3,18 +3,18 @@
         <div class="menu-wrapper">
           <div class="menu-item">
             <a href="mail.php"><i class="fa-regular fa-envelope"></i> Mail</a>
-            <a href="chat.php">Chat's</a>
-            <a href="google.php">Google</a>
-            <a href="aadhar.php">Aadhaar Portal</a>
-            <a href="notepad.php">Notepad</a>
-            <a href="excel.php">Excel</a>
-            <a href="word.php">Word</a>
-            <a href="meet.php">Meet</a>
-            <a href="fileManager.php">File Manager</a>
-            <a href="paint.php">Paint</a>
-            <a href="stickyNotes.php">Stick Notes</a>
-            <a href="powerPoint.php">Power Point</a>
-            <a href="accessRequest.php">Access Request</a>
+            <a href="chat.php"><i class="fa-brands fa-rocketchat"></i> Chat's</a>
+            <a href="google.php"><i class="fa-brands fa-google"></i> Google</a>
+            <a href="aadhar.php"><i class="fa-solid fa-lock"></i> Aadhaar Portal</a>
+            <a href="notepad.php"><i class="fa-regular fa-clipboard"></i> Notepad</a>
+            <a href="excel.php"><i class="fa-regular fa-file-excel"></i> Excel</a>
+            <a href="word.php"><i class="fa-regular fa-file-word"></i> Word</a>
+            <a href="meet.php"><i class="fa-solid fa-desktop"></i> Meet</a>
+            <a href="fileManager.php"><i class="fa-regular fa-file"></i> File Manager</a>
+            <a href="paint.php"><i class="fa-solid fa-brush"></i> Paint</a>
+            <a href="stickyNotes.php"><i class="fa-regular fa-note-sticky"></i> Stick Notes</a>
+            <a href="powerPoint.php"><i class="fa-regular fa-file-powerpoint"></i> Power Point</a>
+            <a href="accessRequest.php"><i class="fa-brands fa-windows"></i> Access Request</a>
             </li>
           </div>
           <span class="pointer left-pointer dis">
