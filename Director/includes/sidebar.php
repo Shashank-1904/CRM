2c6e04667d4c1b775f1096c31ef4bc85a109c6c8
Shashank <shashankumarekar@gmail.com>
2024-02-15 13:32:36 +0530

    logo

--- Director/includes/sidebar.php
+++ Director/includes/sidebar.php
@@ -18,7 +18,7 @@
           <div class="left-arrow" id="left-arrow"><i data-feather="arrow-left"></i></div>
           <div id="sidebar-menu">
             <ul class="sidebar-links" id="simple-bar">
-              <li class="back-btn"><a href="index.html"><img class="img-fluid" src="../assets/images/logo/logo-icon.png"
+              <li class="back-btn"><a href="index.html"><img class="img-fluid" src="../assets/images/logo/main-logo.png"
                     alt=""></a>
                 <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2"
                     aria-hidden="true"></i></div>
